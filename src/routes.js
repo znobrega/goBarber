9335a940fcb2c311b28a4bca64cbd0379f1af825
Carlos Nobrega <nobregacarlosjr@gmail.com>
2019-10-18 03:08:44 -0300

    appointments migration and model

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -24,6 +25,9 @@ routes.get('/providers', ProviderController.index);
 
 routes.put('/users', UserController.update);
 
+routes.post('/appointments', AppointmentController.store);
+
 routes.post('/file', upload.single('file'), FileController.store);
+
 
 export default routes;
